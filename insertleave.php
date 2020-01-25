607bf5b1704635b9c4ab579a37eb69a8c4aca0da
insculpta <rinaafrit@gmail.com>
2020-01-26 04:39:41 +0800

    insertleave

--- insertleave.php
+++ insertleave.php
@@ -1,15 +1,60 @@
 <?php
-$con = mysqli_connect("140.114.55.208","newuser","Tt12345678","postui");
-$id = $_POST['id'];
-$employee = $_POST['employee'];
-$date = $_POST['date'];
-$work = $_POST['work'];
-$sql = "insert into AbsentNote values ('$id', '$employee', '$date', '$work');";
-if(mysqli_query($con, $sql)){
-echo 'success';
+$link = mysqli_connect("192.168.1.170","newuser","123","data");
+$json = file_get_contents('php://input'); 	
+$obj = json_decode($json,true);
+
+
+if (mysqli_connect_errno($link)) {
+      echo "Failed to connect to MySQL: " . mysqli_connect_error();
+	  print(json_encode('Failed to connect'));
+}
+
+
+$EmployeeID = $obj['EmployeeID'];
+$LeaveID = $obj['LeaveID'];		
+$StartDate = $obj['StartDate'];
+$EndDate = $obj['EndDate'];
+$ApplicationDate = $obj['ApplicationDate'];
+$Remark = $obj['Remark'];
+$Audited = $obj['Audited'];
+
+$link -> set_charset("UTF8");
+$sql = "insert into absentNote (EmployeeID,LeaveID,StartDate,EndDate,ApplicationDate,Remark,Audited) values ('$EmployeeID','$LeaveID','$StartDate','$EndDate','$ApplicationDate','$Remark','$Audited');";
+
+
+$result = mysqli_query($link,$sql);
+$insert_result =  mysqli_fetch_array(mysqli_query($link,"SELECT * FROM absentNote where EmployeeID='$EmployeeID' "));
+	
+
+/* while ($res = mysqli_fetch_assoc($result))
+{
+	$output[] = $res;
+}
+
+if (isset($output)){
+	print(json_encode($output,JSON_UNESCAPED_UNICODE));
+
+}
+else if (isset($insert_result)){
+   print(json_encode('apply successfully'));
+}
+
+else{
+   print(json_encode('try again'));
+}
+
+$link -> close(); */
+	
+ 
+if(mysqli_query($link, $sql)){
+ print(json_encode('apply successfully'));
 }
 else{
-echo 'try again';
+ print(json_encode('try again'));
 }
-mysqli_close($con);
+mysqli_close($link); 
+
+
+
+
 ?>
