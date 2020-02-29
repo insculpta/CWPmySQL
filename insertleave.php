e49049d9d8b5bc9a829ecb90954e69a631c66022
insculpta <rinaafrit@gmail.com>
2020-03-01 04:06:13 +0800

    typhoon work

--- insertleave.php
+++ insertleave.php
@@ -1,4 +1,6 @@
 <?php
+
+//$link = mysqli_connect("140.114.55.208","newuser","Tt12345678","postui");
 //$link = mysqli_connect("192.168.1.170","newuser","123","data");
 $link = mysqli_connect("140.114.54.22","newuser","123","data");
 $json = file_get_contents('php://input'); 	
@@ -20,7 +22,7 @@ if (mysqli_connect_errno($link)) {
 
 
 $link -> set_charset("UTF8");
-$sql = "insert into absentNote (EmployeeID,LeaveID,StartDate,EndDate,ApplicationDate,Remark,Audited,Approve) values ('$EmployeeID','$LeaveID','$StartDate','$EndDate','$ApplicationDate','$Remark','$Audited','$Approve');";
+$sql = "insert into absentNote (AbsentNoteID,EmployeeID,LeaveID,StartDate,EndDate,ApplicationDate,Remark,Audited,Approve) values ('218','$EmployeeID','$LeaveID','$StartDate','$EndDate','$ApplicationDate','$Remark','$Audited','$Approve');";
 
 
 $result = mysqli_query($link,$sql);
